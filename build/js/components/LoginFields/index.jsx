9f8f0270a4e98888a52d2d0748f32f4baaa9c38d
Tim Heuermann <tim@tomis.tech>
2019-08-06 20:09:30 -0600

    Update Form layout, styling

--- build/js/components/LoginFields/index.jsx
+++ build/js/components/LoginFields/index.jsx
@@ -9,10 +9,8 @@ export default class LoginFields extends React.Component {
 		}
 		return (
 			<React.Fragment>
-				<fieldset>                
 					<input required className="gecko-input" type="email"  name="email" placeholder="Email Address*" />
 					<input required className="gecko-input" type="password" name="password" placeholder="Password*" />
-				</fieldset>
 				<a className="gecko_sign-up_password" href="#">Forgot Password?</a>
 			</React.Fragment>
 		);
--- build/js/components/SignUpFields/index.jsx
+++ build/js/components/SignUpFields/index.jsx
@@ -8,13 +8,11 @@ export default class LoginFields extends React.Component {
 			this.props.login();
 		}
 		return (
-			<React.Fragment>
-				<fieldset>            
+			<React.Fragment>				        
 					<input required className="gecko-input small" type="text" name="first" placeholder="First Name*" />
 					<input required className="gecko-input small" type="text" name="last" placeholder="Last Name*" />
 					<input required className="gecko-input" type="email" name="email" placeholder="Email Address*" />
-					<input required className="gecko-input" type="password" name="password" placeholder="Set A Password*" />
-				</fieldset>				
+					<input required className="gecko-input" type="password" name="password" placeholder="Set A Password*" />			
 			</React.Fragment>
 		);
 	}
